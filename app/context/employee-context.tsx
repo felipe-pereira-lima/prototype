487441b0f77b379105c5e7a46dc2f7dc362d82ba
fl <59571003+felipe-pereira-lima@users.noreply.github.com>
2023-12-21 16:58:02 +0100

    chore: comment employee context

--- app/context/employee-context.tsx
+++ app/context/employee-context.tsx
@@ -1,32 +1,32 @@
-import { createContext, useContext, ReactNode } from "react";
-import { User } from "@prisma/client";
+// import { createContext, useContext, ReactNode } from "react";
+// import { User } from "@prisma/client";
 
-interface EmployeeContextType {
-  employee: User | null;
-}
+// interface EmployeeContextType {
+//   employee: User | null;
+// }
 
-const EmployeeContext = createContext<EmployeeContextType | undefined>(
-  undefined
-);
+// const EmployeeContext = createContext<EmployeeContextType | undefined>(
+//   undefined
+// );
 
-export function useEmployeeContext() {
-  const context = useContext(EmployeeContext);
-  if (context === undefined) {
-    throw new Error(
-      "useEmployeeContext must be used within a EmployeeContextProvider"
-    );
-  }
-  return context;
-}
+// export function useEmployeeContext() {
+//   const context = useContext(EmployeeContext);
+//   if (context === undefined) {
+//     throw new Error(
+//       "useEmployeeContext must be used within a EmployeeContextProvider"
+//     );
+//   }
+//   return context;
+// }
 
-export const EmployeeContextProvider = ({
-  children,
-  employee,
-}: {
-  children: ReactNode;
-  employee: User | null;
-}) => (
-  <EmployeeContext.Provider value={{ employee }}>
-    {children}
-  </EmployeeContext.Provider>
-);
+// export const EmployeeContextProvider = ({
+//   children,
+//   employee,
+// }: {
+//   children: ReactNode;
+//   employee: User | null;
+// }) => (
+//   <EmployeeContext.Provider value={{ employee }}>
+//     {children}
+//   </EmployeeContext.Provider>
+// );
