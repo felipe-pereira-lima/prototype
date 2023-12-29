da5076da3c7d9da7b487e0425839b6f3206ef6f2
fl <59571003+felipe-pereira-lima@users.noreply.github.com>
2023-12-29 01:06:45 +0100

    chore: refactor file directories

--- app/components/side-bar.tsx
+++ app/components/side-bar.tsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from "react";
 import { Compass } from "phosphor-react";
 import { Link, useLocation } from "@remix-run/react";
 import clsx from "clsx";
-import {
-  NavigationItem,
-  navigationList,
-} from "./utils/side-bar-navigation-list";
+import { navigationList } from "./side-bar-navigation-list";
+import { NavigationItem } from "~/utils/types";
 
 export default function ApplicationSidebar(): JSX.Element {
   const location = useLocation();
@@ -50,61 +48,59 @@ export default function ApplicationSidebar(): JSX.Element {
     };
 
     setActiveItem(findActiveItem(navigationList));
-  }, [location, navigationList]);
+  }, [location]);
 
   return (
     <>
-      <div>
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-            <Link to="/">
-              <div className="flex h-16 shrink-0 items-center gap-x-1">
-                <Compass weight="fill" size={28} />
-                <p className="font-bold">Crescendo</p>
-              </div>
-            </Link>
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+          <Link to="/">
+            <div className="flex h-16 shrink-0 items-center gap-x-1">
+              <Compass weight="fill" size={28} />
+              <p className="font-bold">Crescendo</p>
+            </div>
+          </Link>
 
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-2">
-                {navigationList.map((item) => (
-                  <li
-                    key={item.name}
-                    className={clsx(
-                      activeMainItem === item.name && "bg-gray-100 rounded-md"
-                    )}
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-2">
+              {navigationList.map((item) => (
+                <li
+                  key={item.name}
+                  className={clsx(
+                    activeMainItem === item.name && "bg-gray-100 rounded-md"
+                  )}
+                >
+                  <Link
+                    to={item.href}
+                    className="group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-700 hover:text-blue-500 hover:bg-gray-100"
+                    onClick={() => handleMenuClick(item)}
                   >
-                    <Link
-                      to={item.href}
-                      className="group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-gray-700 hover:text-blue-500 hover:bg-gray-100"
-                      onClick={() => handleMenuClick(item)}
-                    >
-                      {item.name}
-                    </Link>
-                    {openedMenu === item.name && item.submenu && (
-                      <ul role="list" className="pl-4 space-y-1 rounded-md">
-                        {item.submenu.map((subItem) => (
-                          <li key={subItem.name}>
-                            <Link
-                              to={subItem.href}
-                              className={clsx(
-                                activeItem === subItem.name
-                                  ? "text-blue-500"
-                                  : "text-gray-700 hover:text-blue-500 hover:bg-gray-100",
-                                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                              )}
-                              onClick={() => setActiveItem(subItem.name)}
-                            >
-                              {subItem.name}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+                    {item.name}
+                  </Link>
+                  {openedMenu === item.name && item.submenu && (
+                    <ul role="list" className="pl-4 space-y-1 rounded-md">
+                      {item.submenu.map((subItem) => (
+                        <li key={subItem.name}>
+                          <Link
+                            to={subItem.href}
+                            className={clsx(
+                              activeItem === subItem.name
+                                ? "text-blue-500"
+                                : "text-gray-700 hover:text-blue-500 hover:bg-gray-100",
+                              "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                            )}
+                            onClick={() => setActiveItem(subItem.name)}
+                          >
+                            {subItem.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
       </div>
     </>
