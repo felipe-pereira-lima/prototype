d33f2c8905faa5a75dd1919cf25eee4e0c6b10f2
fl <59571003+felipe-pereira-lima@users.noreply.github.com>
2024-01-30 23:05:33 +0100

    fix: styling of logout button

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -50,7 +50,7 @@ export default function Navbar(): JSX.Element {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="absolute text-right right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none">
+                      <Menu.Items className="absolute text-right right-0 z-10 mt-2 w-fit origin-top-right rounded-md bg-white py-1 shadow-lg focus:outline-none">
                         <Menu.Item>
                           <Form method="post">
                             <Button type="submit" variant="link">
